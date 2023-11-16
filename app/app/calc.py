6f4458ca0330d9860c905746616486f538795875
Pramod Singh Manyal <manyalp12345@gmail.com>
2023-11-16 15:26:00 +0545

    Installatio of the physcobe2psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,6 @@
+"""
+Calculater function
+"""
+
+def add(x, y):
+    return x + y
--- app/app/settings.py
+++ app/app/settings.py
@@ -123,3 +123,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'db',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'user',
+        'PASSWORD': 'password',
+
+    }
+}
