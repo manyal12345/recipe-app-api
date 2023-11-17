455e46fa4220c803c66dc3a24ce83db8fbb51b2c
Pramod Singh Manyal <manyalp12345@gmail.com>
2023-11-17 20:41:34 +0545

    Super user Create and test

--- app/app/settings.py
+++ app/app/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        
+
     }
 }
 
@@ -129,4 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
 
