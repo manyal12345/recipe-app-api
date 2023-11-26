78d513ebd42f71d7749aeb839516b85056a5dfa3
Pramod Singh Manyal <manyalp12345@gmail.com>
2023-11-26 08:59:45 +0545

    Build Image API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,5 +1,11 @@
 """ Views For the Recipe API """
-from rest_framework import mixins, viewsets
+from rest_framework import (
+    mixins,
+    viewsets,
+    status,
+)
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -29,6 +35,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeImageSerializer
 
         return self.serializer_class
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
         """Create a new recipe."""
